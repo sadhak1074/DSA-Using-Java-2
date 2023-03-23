765a25838fa06a6edd3ce89073c53f8d4c783b6c
Sadhak Saini <sadhaksaini18@gmail.com>
2023-03-24 00:11:46 +0530

    array 3rd

---
+++ array3.java
@@ -0,0 +1,11 @@
+public class array3 {
+    public static void main(String[] args) {
+        int [] main = new int[10];
+        for (int i = 0; i < main.length; i++) {
+            main[i]=i;
+        }
+        for (int i = 0; i < main.length; i++) {
+            System.out.println(main[i]);
+        }
+    }
+}
