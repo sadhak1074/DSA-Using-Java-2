993e893234a3153ce9f257e149f5506636d28ce4
Sadhak Saini <sadhaksaini18@gmail.com>
2023-03-24 00:08:31 +0530

    Array 1st

---
+++ array1.java
@@ -0,0 +1,11 @@
+public class array1{
+    public static void main(String[] args) {
+        int [] main = new int[10];
+        main[0]=1;
+        main[1]=2;
+        main[2]=3;
+        main[3]=4;
+        System.out.println(main[3]);
+        
+    }
+}
