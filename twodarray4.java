54668ef103bb9816beee4aebc6ba6854842809f7
Sadhak Saini <sadhaksaini18@gmail.com>
2023-03-27 00:40:35 +0530

    arrays

---
+++ twodarray4.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class twodarray4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Get the size of the matrix
+        System.out.print("Enter the number of rows: ");
+        int rows = input.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int cols = input.nextInt();
+
+        // Create the matrix
+        int[][] matrix = new int[rows][cols];
+
+        // Get the elements of the matrix
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = input.nextInt();
+            }
+        }
+
+        // Transpose the matrix
+        int[][] transposeMatrix = new int[cols][rows];
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                transposeMatrix[i][j] = matrix[j][i];
+            }
+        }
+
+        // Print the transpose matrix
+        System.out.println("Transpose of the matrix:");
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(transposeMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
